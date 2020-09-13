a4645cbd845b3befd49392b7b894dc21b50093ad
Tawkir Talukder <ubiquitouss@yahoo.com>
2020-09-13 20:07:41 +0600

    log in is completed..aadn after login loggout appears

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'authenticate/home.html',{})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,('You have been logged in'))
+            return redirect('home')
+
+        else:
+            messages.success(request,('Error logging in! Please try again'))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html',{})
+
