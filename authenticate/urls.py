c0cd88f65d11009202b16d681bfbdf2d1e69c82d
Tawkir Talukder <ubiquitouss@yahoo.com>
2020-09-16 11:19:50 +0600

    password reset completed

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path 
+from django.urls import path ,include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -8,6 +10,20 @@ urlpatterns = [
     path('logout/',views.logout_user, name='logout'),
     path('register/',views.register_user, name='register'),
     path('edit_profile/',views.edit_profile, name='edit_profile'),
+
     path('change_password/',views.change_password, name='change_password'),
     path('activate/<uidb64>/<token>/',views.activate, name='activate'),
+    
+    
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'), name='password_reset_complete'),  
+
+    path("password_reset/", views.password_reset_request, name="password_reset"),    
+
+
+
+
+#     path('',include('django.contrib.auth.urls')) 
+
 ]
--- authenticate/views.py
+++ authenticate/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout,update_session_auth_hash
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm, PasswordResetForm
 from django.contrib import messages
 from .forms import SignUpForm,EditProfileForm
 
@@ -9,12 +9,13 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, send_mail, BadHeaderError
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.db.models.query_utils import Q
 UserModel = get_user_model()
 from .forms import SignUpForm
 # from .tokens import account_activation_token
@@ -120,3 +121,30 @@ def change_password(request):
     context={'form':form} 
     return render(request, 'authenticate/change_password.html', context)
 
+def password_reset_request(request):
+	if request.method == "POST":
+		password_reset_form = PasswordResetForm(request.POST)
+		if password_reset_form.is_valid():
+			data = password_reset_form.cleaned_data['email']
+			associated_users = User.objects.filter(Q(email=data))
+			if associated_users.exists():
+				for user in associated_users:
+					subject = "Password Reset Requested"
+					email_template_name = "password/password_reset_email.html"
+					c = {
+					"email":user.email,
+					'domain':'127.0.0.1:8000',
+					'site_name': 'Website',
+					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					"user": user,
+					'token': default_token_generator.make_token(user),
+					'protocol': 'http',
+					}
+					email = render_to_string(email_template_name, c)
+					try:
+						send_mail(subject, email, 'tawkir.ph@gmail.com' , [user.email], fail_silently=False)
+					except BadHeaderError:
+						return HttpResponse('Invalid header found.')
+					return redirect ("/password_reset/done/")
+	password_reset_form = PasswordResetForm()
+	return render(request=request, template_name="password/password_reset.html", context={"password_reset_form":password_reset_form})
